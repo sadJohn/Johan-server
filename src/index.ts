34ce1b5466e9379260eb414a86751dcd6f3e2626
Johan <1938159279@qq.com>
2024-12-28 12:16:34 +0800

    db

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import { Hono } from "hono";
+import { db } from "./db";
+import { usersTable } from "./db/schema";
 
 const app = new Hono();
 
-app.get("/", (c) => {
-  return c.text("Hello Hono!");
+app.get("/", async (c) => {
+  const users = await db.select().from(usersTable);
+  return c.json(users);
 });
 
 export default app;
